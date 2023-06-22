4fd1d2e9434bea04ace501ff88e48d5959f5290f
Tina Shi <init.defaultBranch>
2023-06-22 19:46:29 +1000

    commit tree 6f23486d5c587136db5c10342991e2c53f982e4c

--- db/migrate/20210506153127_create_rides.rb
+++ db/migrate/20210506153127_create_rides.rb
@@ -1,7 +1,8 @@
 class CreateRides < ActiveRecord::Migration[6.1]
   def change
     create_table :rides do |t|
-
+      t.belongs_to :taxi, foreign_key: true
+      t.belongs_to :passenger, foreign_key: true
       t.timestamps
     end
   end
